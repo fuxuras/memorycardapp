db71c147c48fae244cc5e0113c2bec09730015b9
fokal <furkancan.okal@gmail.com>
2024-08-28 20:17:19 +0300

    feat: MemorycardsApplication.java

---
+++ src/main/java/com/fuxuras/memorycards/MemorycardsApplication.java
@@ -0,0 +1,13 @@
+package com.fuxuras.memorycards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemorycardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemorycardsApplication.class, args);
+    }
+
+}
