e2f10c8fe4e805289c9c294aabac9ef308279c92
fuxuras <furkancanokal1@gmail.com>
2024-10-15 00:08:12 +0300

    feat: create member

--- src/main/java/com/fuxuras/memorycards/services/MemberService.java
+++ src/main/java/com/fuxuras/memorycards/services/MemberService.java
@@ -5,6 +5,8 @@ import com.fuxuras.memorycards.repositories.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 @RequiredArgsConstructor
 public class MemberService {
@@ -12,5 +14,16 @@ public class MemberService {
 
     public Member getMemberByUsername(String name) {
         return memberRepository.getMemberByUsername(name);
+    }
+
+    public boolean createMember(String username, String password) {
+        if (memberRepository.existsByUsername(username)) {
+            return false;
+        }
+        Member member = new Member();
+        member.setUsername(username);
+        member.setPassword(password);
+        memberRepository.save(member);
+        return true;
     }
 }
